43f28f13242ef4b270c7238c9768b16dae51da52
Jerry <minemaster151@gmail.com>
2024-03-25 10:04:40 +0700

    added some logo

---
+++ app/components/Skill.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+interface SkillProps {
+  label: string;
+  icon: string;
+}
+
+export const Skill: React.FC<SkillProps> = ({ label, icon }) => {
+  return (
+    <div className="bg-blue-500 block text-center p-4 rounded-lg m-2 hover:animate-[slowfloating_3s_infinite] hover:shadow-lg">
+      <div className="flex justify-center items-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+        {icon}
+      </div>
+      <div className="text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] mt-4">
+        {label}
+      </div>
+    </div>
+  );
+};
